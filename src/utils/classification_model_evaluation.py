e5872309841f60d85703d5a6840e657862eac6f7
Gustavo Suto <guyrux@gmail.com>
2020-09-14 00:52:26 -0300

    [feat] Feita mais uma avaliação (F1-Micro = 0,55).
    - No notebook 3_modeling_suto, deixamos o protótico de uma função para armazenar e salvar a avaliação do modelo.
    - Iniciamos a criação de uma classe (classification_model_evaluation.py) que conterá a função descrita no item anterior.

---
+++ src/utils/classification_model_evaluation.py
@@ -0,0 +1,11 @@
+class ClassificationModelEvaluation:
+    """
+    [Essa classe implementa funções que nos ajudarão a medir e armazenar o resultado das avaliações
+    de modelos de classificação]
+    """
+
+    def __init__(self):
+        pass
+
+    def model_evaluation():
+        pass
--- src/utils/data_describe.py
+++ src/utils/data_describe.py
@@ -10,15 +10,17 @@ class DataDescribe:
     def __init__(self):
         pass
 
+
     def breve_descricao(df):
         """
-        Função breveDescricao
+        Função breve_descricao
         
         Objetivo: Exclui atributos que estejam com colunas com todos os valores
         'NaN'. Imprime na tela a quantidade de atributos/campos e a quantidade
         de registros.
         
-        Argumentos: um DataFrame.
+        Args:
+            df ([pandas.DataFrame]): [Dataframe que queremos analisar.]
         """
         
         df.dropna(axis=1, how="all", inplace=True)
@@ -28,32 +30,31 @@ class DataDescribe:
         df.info()
     
     
-    
-    def serie_nulos(df, corte = 50):
+        
+    def serie_nulos(df, corte = 0.5):
         """
-         responsável: Suto
-         data: 04/05/19
+        Função serie_nulos
+
+        Responsável: Suto
+        Data: 04/05/19
         
-        objetivo: essa função retorna uma tupla com:
-                 (1) contendo uma pd.series com os atributos com maior
-                 proporção de nulos; e
-                 (2) uma string indicando quantos atributos estão com uma
-                 proporção de nulos acima do corte dado.
-                 argumentos: são necessários 2 argumentos:
-                 (1) dataframe df - base que se deseja verificar quantos nulos
-                 têm; e
-                 (2) float corte - número entre 0 e 100 para indicar o valor
-                 mínimo da proporção de nulos. 
+        Objetivo: essa função retorna uma tupla com:
+            (1) contendo uma pd.series com os atributos com maior proporção de nulos; e
+            (2) uma string indicando quantos atributos estão com uma proporção de nulos acima do corte dado.
         
-        Argumentos: Um DataFrame e um corte em porcentagem (valor padrão é 50%).
-        
+        Args:
+            df ([pandas.DataFrame]): [Dataframe que queremos analisar.]
+            corte (int, optional): [Limite mínimo de nulos presentes em um atributo para destacar]. Default = 50.
+
+        Returns:
+            [pandas.DataFrame]: [DataFrame contendo os atributos que possuem uma proporção de nulos acima ]
         """
         
-        serie = (100*df.isnull().sum().sort_values(ascending=False)/len(df))
+        serie = (df.isnull().sum().sort_values(ascending=False)/len(df))
         
         serie_cortada = serie[serie > corte]
 
-        print(f"{len(serie_cortada)} atributos/features/campos possuem mais de {corte}% de valores nulos.")
+        print(f"{len(serie_cortada)} atributos/features/campos possuem mais de {corte} de valores nulos.")
         
         return serie_cortada
     
@@ -68,8 +69,9 @@ class DataDescribe:
         Argumentos: somente 01 (um) argumento, o DataFrame que se deseja
                     analisar.
         """
-        import pandas as pd
 
+        import pandas as pd
+        
         df_temporario = df.select_dtypes(exclude=["float64"])
 
         matriz_cardialidade = []
@@ -81,7 +83,7 @@ class DataDescribe:
         matriz_cardialidade.sort_values(by=["Cardinalidade", "Atributo"], inplace=True, ascending=True)
         
         return matriz_cardialidade
-    
+
 
 
     def cardinalidade_com_descricao(df):
@@ -95,10 +97,11 @@ class DataDescribe:
             cardinalidade em ordem crescente. 
         argumentos: somente 01 (um) argumento, o DataFrame que se deseja analisar.
         """
+
         import pandas as pd
         
         df_temporario = df.select_dtypes(exclude=["int64", "float64"])
-
+        
         matriz_cardialidade = []
 
         for i, coluna in df_temporario.items():
@@ -119,6 +122,7 @@ class DataDescribe:
         r2_ajustado retorna o R² Ajustado e recebe como argumento as séries com
         o valor alvo teste e o predito.
         """
+
         from sklearn.metrics import r2_score
         
         n = x.shape[0]
